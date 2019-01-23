a4147eea8919fac6f7f3d777291024b108444704
Kristoffer Henden Kaasen <sondrehf@stud.ntnu.no>
2019-01-23 12:57:04 +0100

    day two

--- Dag_2/Part2/go/foo.go
+++ Dag_2/Part2/go/foo.go
@@ -19,6 +19,17 @@ func number_server(add_number <-chan int, control <-chan int, number chan<- int)
 	// This for-select pattern is one you will become familiar with if you're using go "correctly".
 	for {
 		select {
+			case n := <- add_number:
+				i+=n
+			case c := <- control:
+				if c == GetNumber{
+					number <- i
+				}else if c == Exit{
+					return
+				}
+			
+
+
 			// TODO: receive different messages and handle them correctly
 			// You will at least need to update the number and handle control signals.
 		}
@@ -30,24 +41,37 @@ func incrementing(add_number chan<-int, finished chan<- bool) {
 		add_number <- 1
 	}
 	//TODO: signal that the goroutine is finished
+	finished <- true
 }
 
 func decrementing(add_number chan<- int, finished chan<- bool) {
-	for j := 0; j<1000000; j++ {
+	for j := 0; j<1000000-3; j++ {
 		add_number <- -1
 	}
 	//TODO: signal that the goroutine is finished
+	finished <- true
 }
 
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	// TODO: Construct the required channels
+	add_number := make(chan int) //we have chosen the default unbuffered channels 
+	control := make(chan int) 
+	number := make(chan int)
+	finished := make(chan bool)  
 	// Think about wether the receptions of the number should be unbuffered, or buffered with a fixed queue size.
 
 	// TODO: Spawn the required goroutines
+	go number_server(add_number, control, number)
+	go incrementing(add_number, finished)
+	go decrementing(add_number, finished)
 
 	// TODO: block on finished from both "worker" goroutines
+	
+	//Waiting for finish
+	<- finished
+	<- finished
 
 	control<-GetNumber
 	Println("The magic number is:", <- number)
--- Dag_2/Part2/python/foo.py
+++ Dag_2/Part2/python/foo.py
@@ -1,30 +1,44 @@
 # Python 3.3.3 and 2.7.6
 # python fo.py
 
-from threading import Thread
-
+#from threading import Thread
+import threading
 # Potentially useful thing:
 #   In Python you "import" a global variable, instead of "export"ing it when you declare it
 #   (This is probably an effort to make you feel bad about typing the word "global")
 i = 0
+lock = threading.Lock()
 
 def incrementingFunction():
     global i
+    global lock
     # TODO: increment i 1_000_000 times
+    for j in range(0,1000000):
+        lock.acquire()
+        i += 1
+        lock.release()
+
 
 def decrementingFunction():
     global i
+    global lock
     # TODO: decrement i 1_000_000 times
+    for j in range(0,1000000):
+        lock.acquire()
+        i -= 1
+        lock.release()
 
 
 
 def main():
     global i
 
-    incrementing = Thread(target = incrementingFunction, args = (),)
-    decrementing = Thread(target = decrementingFunction, args = (),)
+    incrementing = threading.Thread(target = incrementingFunction, args = (),)
+    decrementing = threading.Thread(target = decrementingFunction, args = (),)
     
     # TODO: Start both threads
+    incrementing.start()
+    decrementing.start()
     
     incrementing.join()
     decrementing.join()
